b303d7bc0c378633e100fa76f0e6e6b49a712bcf
Michael Lim <dlim2010@gmail.com>
2014-05-06 22:54:03 -0700

    Starting FCFS

--- src/Driver.java
+++ src/Driver.java
@@ -1,18 +1,87 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 
 
 
 public class Driver {
 	//Test
+	
+	public int numCPU;
+	public int algorithm;
+	public ArrayList<Job> jobs;
+	
+	public Driver(int numCPU, int algorithm){
+		this.numCPU= numCPU;
+		this.algorithm = algorithm;
+		this.jobs = new ArrayList<Job>();
+	}
+	
+	public void processJobs(ArrayList<Job> jobs){
+		LinkedList<Job> toProcess = new LinkedList<Job>();
+		
+		Job currJob;
+		long currentTime;
+		long startTime = System.currentTimeMillis(); //Get start time
+		
+		while(!jobs.isEmpty()){// While we have Jobs to add
+			currentTime = System.currentTimeMillis(); //Get the current time, and set the Job's "Enter time" 
+			currJob = jobs.remove(0);
+			currJob.enterTime = currentTime;
+			toProcess.offer(currJob);// Add the job to our list to be processed
+			
+		}
+	}
 
 	public static void main(String[] args) {
+		Driver d;
+		int numCPU;
+		int algorithm;
+		String filename;
+		
+		if(args.length < 3){
+			numCPU = 3;
+			algorithm = 1;
+			filename = "input.txt";
+		}
+		else{
+			//Parse number of CPUs
+			numCPU = Integer.parseInt(args[0]);
+			//Parse algorithm selected
+			algorithm = Integer.parseInt(args[1]);
+			filename = args[2];
+		}
+		
+		d = new Driver(numCPU, algorithm);
+		
 		//Read in text file
-		
-		//Parse number of CPUs
-		
-		//Parse algorithm selected
-		
 		//Parse jobs file
-		
+		 
+		try(Scanner file = new Scanner(new FileReader(new File(filename)));){ //Open file
+			
+			String nextLine;
+			d = new Driver(numCPU, algorithm);
+			while(file.hasNextLine()){//While we still have lines
+				nextLine = file.nextLine(); //Read in the next line
+				
+				int nextProc = (int)(nextLine.charAt(0))-48; //Parse info from file
+				char nextType = nextLine.charAt(1);
+				int nextLength = Integer.parseInt(nextLine.substring(2));
+				
+				Job j = new Job(nextProc, nextType, nextLength ); //Create a new job
+				d.jobs.add(j);
+				System.out.println("Made "+j);
+			}
+		}
+		catch(IOException e){
+			System.err.println("File Error, check file name");
+			System.exit(-1);
+		}
 		
 
 	}
--- src/Job.java
+++ src/Job.java
@@ -2,7 +2,36 @@
 public class Job {
 	public enum Type {CPU, IO}
 	
-	public Type type;
-	public int length;
+	public int proc;
+	public char type;
+	public int timeNeeded;
+	public long enterTime;
+	public long firstAddressedTime;
+	public long finishTime;
+	
+	public Job(int proc, char type, int timeNeeded){
+		this.proc = proc;
+		this.type = type;
+		this.timeNeeded = timeNeeded;
+		this.firstAddressedTime = -1;
+		this.enterTime = -1;
+		this.finishTime = -1;
+	}
+	
+	public String getTypeString(){
+		if (this.type == 'C'){
+			return "CPU";
+		}
+		else if ( this.type == 'I'){
+			return "IO";
+		}
+		else{
+			return "Unknown";
+		}
+	}
+	
+	public String toString(){
+		return "A "+getTypeString()+" job for process "+ this.proc+", started at "+this.enterTime+", and needs " + this.timeNeeded;
+	}
 	
 }
